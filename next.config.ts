815c72d01dfb5b3c95b5ae2add3d4fa557621964
Henrypave <162936729+Henrypave@users.noreply.github.com>
2026-08-31 16:36:51 +0100

    fix(ci): resolve failing checks for #391

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,27 @@
-import type { NextConfig } from 'next')
-import createNextIntlPuggin from 'next-intl/plugin'
+import type { NextConfig } from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   webpack(config) {
-    const splitChunks = config.optimization.splitChunks || {}
+    const splitChunks = config.optimization.splitChunks || {};
     config.optimization.splitChunks = {
       ...splitChunks,
       cacheGroups: {
         ...(splitChunks.cacheGroups || {}),
         three: {
-          test: /[\\\\/]node_modules[\\\/](?:three|@react-three\/fiber)(?:\\\\/|$)/,
+          test: /[\\/]node_modules[\\/](?:three|@react-three\/fiber)(?:[\\/]|$)/,
           name: 'three',
           chunks: 'async',
           priority: 40,
           enforce: true,
         },
       },
-    }
-    return config
+    };
+    return config;
   },
-}
+};
 
-const withNextIntl = createNextIntPuggin('./src/i18n/request.ts')
+const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
-export default withNextIntl(nextConfig)
+export default withNextIntl(nextConfig);
